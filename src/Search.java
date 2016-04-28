bd42ca726b0de0a3b02fbbb1add8af0c4eab7c6d
dariavladimirovna <dulya-1803@mail.ru>
2016-04-28 23:30:11 +0300

    Добавлен поиск глаголов и наречий в файле, удалены лишние строки

--- src/Search.java
+++ src/Search.java
@@ -13,22 +13,18 @@ public class Search {
 
     public static void main(String[] args)
     {
-        int count = 0;
         int count_p1 = 0;
         int count_p2 = 0;
         int count_p3 = 0;
-        int count_p4 = 0;
-        int count_p5 = 0;
-        char st='Ы';
 
-        Pattern pattern = Pattern.compile("\\w+");
-        Pattern p1 = Pattern.compile("[а-яА-ЯёЁ]{4,}"); // длиннее 3 символов
-        Pattern p2 = Pattern.compile("\\b\\w*[aeiouAEIOU]\\b"); // заканчиваются на гласную
-        Pattern p3 = Pattern.compile("\\b\\w*[aeiou]{2,}\\w*\\b"); // встречаются две и более гласные подряд
-        Pattern p4 = Pattern.compile("\\b\\w*(\\w)(\\1)\\w*\\b"); // встречаются две одинаковые подряд буквы
+        Pattern p1 = Pattern.compile("\\b[а-яА-ЯёЁ]+(ый|ой|ий|ая|яя|ое|ее|ые|ие|ого|его|ому|" +
+        "ему|ую|юю|им|ым|ом|ем|ых|их|ыми|ими)\\b"); // прилагательные
 
-        Pattern p5 = Pattern.compile("\\b[а-яА-ЯёЁ]+(ый|ой|ий|ая|яя|ое|ее|ые|ие|ого|его|ому|" +
-                "ему|ую|юю|им|ым|ом|ем|ых|их|ыми|ими)\\b"); // встречаются две одинаковые подряд буквы
+        Pattern p2 = Pattern.compile("\\b[а-яА-ЯёЁ]+(у|ю|ешь|ёшь|ет|ёт|ем|ём|ете|ёте|ут|ют|" +
+                "ишь|ит|им|ите|ат|ят)\\b"); // глаголы
+
+        Pattern p3 = Pattern.compile("\\b(в|за|на)[а-яА-ЯёЁ]+(о)\\b"); // наречия
+        Pattern p4 = Pattern.compile("\\b(до|из|с)[а-яА-ЯёЁ]+(а)\\b"); // наречия
 
         BufferedReader in;
         try
@@ -38,8 +34,6 @@ public class Search {
             String s;
             while ((s = in.readLine()) != null)
             {
-                Matcher matcher = pattern.matcher(s);
-                while (matcher.find()) count++;
 
                 Matcher matcher1 = p1.matcher(s);
                 while (matcher1.find()) count_p1++;
@@ -51,10 +45,7 @@ public class Search {
                 while (matcher3.find()) count_p3++;
 
                 Matcher matcher4 = p4.matcher(s);
-                while (matcher4.find()) count_p4++;
-
-                Matcher matcher5 = p5.matcher(s);
-                while (matcher5.find()) count_p5++;
+                while (matcher4.find()) count_p3++;
             }
 
 
@@ -63,18 +54,11 @@ public class Search {
             e.printStackTrace();
         }
 
-        System.out.println("Количество слов: " + count);
+        System.out.println("Количество прилагательных: " + count_p1);
 
-        System.out.println("Количество слов длиннее 3 символов: " + count_p1);
+        System.out.println("Количество глаголов: " + count_p2);
 
-        System.out.println("Количество слов, закачивающихся на гласную: " + count_p2);
-
-        System.out.println("Количество слов в которых встречаются две и более гласные подряд: " + count_p3);
-
-        System.out.println("Количество слов, в котором встречаются две одинаковые подряд буквы: " + count_p4);
-
-        System.out.println("Количество прилагательных: " + count_p5);
-        System.out.println((int)st);
+        System.out.println("Количество наречий: " + count_p3);
 
     }
 
